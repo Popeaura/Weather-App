ad3402b07d27829a0259c0392cb9340325589717
popeaura <popeahura@gmail.com>
2025-02-12 11:41:40 +0300

    I added my modified project's folder

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 const apiKey = "d3e27ab0870547137c7a08aa4a1f0dc8";
-const apiUrl= "https://api.openweathermap.org/data/2.5/weather?q=Nairobi&units=metric";
+const apiUrl= "https://api.openweathermap.org/data/2.5/weather?&units=metric";
