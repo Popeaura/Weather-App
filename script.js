9d7cd4ce7ad71377d313e90de4bb095c90be8d28
popeaura <popeahura@gmail.com>
2025-02-12 11:55:38 +0300

    I added my modified project's folder

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const apiKey = "d3e27ab0870547137c7a08aa4a1f0dc8";
-const apiUrl= "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=Abudhabi";
+const apiUrl= "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=Nairobi";
 
 async function checkWeather(){
     const response = await fetch(apiUrl + `&appid=${apiKey}`)
     var data = await response.json();
 
     console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+
+    document.querySelector(".temp").innerHTML = data.main.temp;
+    document.querySelector(".humidity").innerHTML = data.main.humidity;
+    document.querySelector(".wind").innerHTML = data.wind.speed;
+
+
 }
+
+checkWeather();
